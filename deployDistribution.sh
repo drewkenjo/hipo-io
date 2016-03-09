4eabd11db62a2164531af664ec307612592ec907
Gagik Gavalian <gavalian@meson.jlab.org>
2016-03-09 10:49:46 -0500

    deploy distribution shell command is added

---
+++ deployDistribution.sh
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------------------------
+# Script is exporting existing Jar files to repository
+#-------------------------------------------------------------------------------------------------
+#  JEVIO
+REPO="/Users/gavalian/Work/MavenRepo"
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=target/hipo-1.0.jar \
+    -DgroupId=org.hep.hipo \
+    -DartifactId=hipo \
+    -Dversion=1.0-SNAPSHOT \
+    -Dpackaging=jar \
+    -DlocalRepositoryPath=$REPO
+
+scp -r $REPO/org/hep/hipo/hipo clas12@jlabl1:/group/clas/www/clasweb/html/clas12maven/org/hep/hipo/.
